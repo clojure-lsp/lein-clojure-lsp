ac0c65445c99953aeab7561a64f8fa24b68c1654
Eric Dallo <ericdallo06@hotmail.com>
2021-08-16 09:48:04 -0300

    Bump clojure-lsp

--- project.clj
+++ project.clj
@@ -5,4 +5,4 @@
             :url "https://opensource.org/licenses/MIT"}
   :eval-in-leiningen true
   :pedantic? :warn
-  :dependencies [[com.github.clojure-lsp/clojure-lsp "2021.08.05-19.36.09"]])
+  :dependencies [[com.github.clojure-lsp/clojure-lsp "2021.08.16-12.43.38"]])
